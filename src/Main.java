8204b6103dc0a90e862d28569ef9bf9c4a7c31e4
bruce <l4m.bruc3@gmail.com>
2026-02-24 15:21:26 -0500

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 import Task1.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        double[][] numss = new double[][]{{1,2,3}, {4,5,6}, {7,8,9}};
-        Array2DUtils.min(numss);
+        double[][] numss = new double[][]{
+                {1,5,8},
+                {3,2,6,16,21,5,1,463,21,5413,5},
+                {9,4,1},
+        };
+        System.out.println(Array2DUtils.min(numss));
+
+        System.out.println(Arrays.toString(Array2DUtils.minOfRow(numss)));
+        System.out.println(Arrays.toString(Array2DUtils.minOfCol(numss)));
     }
 }
--- src/Task1/Array2DUtils.java
+++ src/Task1/Array2DUtils.java
@@ -1,5 +1,7 @@
 package Task1;
 
+import java.util.Arrays;
+
 public class Array2DUtils {
 
     // 1.
@@ -7,20 +9,63 @@ public class Array2DUtils {
         double min = Double.MAX_VALUE;
         for (double[] nums : numss) {
             for (double num : nums) {
-                if (num < min) {
+                if (num < min)
                     min = num;
-                }
             }
         }
-        System.out.println(min);
         return min;
     }
 
     // 2.
     static public double[] minOfRow(double[][] numss) {
-        double min = Double.MAX_VALUE;
+        double min;
+        double[] minOfRow = new double[numss.length];
+        int i = 0;
         for (double[] nums : numss) {
+            min = Double.MAX_VALUE;
+            for (double num : nums) {
+                if (num < min)
+                    min = num;
 
+            }
+            minOfRow[i] = min;
+            i++;
+        }
+        return minOfRow;
+    }
+
+    // 3.
+    static public double[] minOfCol(double[][] numss) {
+        int length = Integer.MIN_VALUE;
+        for (double[] nums : numss) {
+            if (nums.length > length) length = nums.length;
+        }
+
+        double[][] swap = new double[length][length];
+        for (double[] minOfCol : swap) {
+            Arrays.fill(minOfCol, Double.POSITIVE_INFINITY);
+        }
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                try {
+                    swap[i][j] = numss[j][i];
+                } catch (IndexOutOfBoundsException _) {
+                }
+
+            }
+        }
+        return minOfRow(swap);
+    }
+
+        // 4.
+    static public double[][] add(double[][] numss1, double[][] numss2) {
+        double add1;
+        double add2;
+        for (double[] ns1 : numss1) {
+            for (double n1 : ns1) {
+
+                }
+            }
         }
     }
 }
